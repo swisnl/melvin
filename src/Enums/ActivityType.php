602e7e1fd5033ed56b6a9905be2fa5312b9c1fcb
Jasper Zonneveld <jasper@swis.nl>
2021-11-30 14:13:27 +0100

    Update ActivityType enum

--- src/Enums/ActivityType.php
+++ src/Enums/ActivityType.php
@@ -10,19 +10,22 @@ use MyCLabs\Enum\Enum;
  * @method static ActivityType WORK()
  * @method static ActivityType EVENT()
  * @method static ActivityType WATERWAY()
+ * @method static ActivityType WATERWAY_EVENT()
  */
 final class ActivityType extends Enum
 {
     private const WORK = 'WORK';
     private const EVENT = 'EVENT';
     private const WATERWAY = 'WATERWAY';
+    private const WATERWAY_EVENT = 'WATERWAY_EVENT';
 
     public function getLabel(): string
     {
         return [
             'WORK' => 'Werk',
             'EVENT' => 'Evenement',
-            'WATERWAY' => 'Vaarwegen',
+            'WATERWAY' => 'Vaarweg werk',
+            'WATERWAY_EVENT' => 'Vaarweg evenement',
         ][$this->getKey()];
     }
 }
