5c463f06a488fe513133895d21b4370c2d03242c
marinusvanvelzen <rvanvelzen@swis.nl>
2024-11-01 10:36:24 +0100

    fix: allow city property of Location to be null

--- src/Models/Location.php
+++ src/Models/Location.php
@@ -7,7 +7,7 @@ namespace Swis\Melvin\Models;
 class Location
 {
     public function __construct(
-        public string $city,
+        public ?string $city,
         public string $road,
         public ?string $district,
         public string $comment,
